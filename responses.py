160d5c1865f81c0ca67ac32bf7d4e1e48aef48a4
km222uq <km222uq@student.lnu.se>
2023-12-21 06:00:36 +0100

    Add requirements.txt, .gitignore, .env.example, and README.md files

--- responses.py
+++ responses.py
@@ -4,28 +4,9 @@ from time import sleep
 from dotenv import load_dotenv
 import requests
 import os
-from transformers import AutoModelForCausalLM, AutoTokenizer
 
 load_dotenv()
 client = OpenAI()
-
-
-def Phi2Response(prompt):
-    import torch
-
-    torch.set_default_device("cuda")
-
-    model = AutoModelForCausalLM.from_pretrained("microsoft/phi-2", torch_dtype="auto", trust_remote_code=True)
-    tokenizer = AutoTokenizer.from_pretrained("microsoft/phi-2", trust_remote_code=True)
-
-    inputs = tokenizer('''def print_prime(n):
-    """
-    Print all primes between 1 and n
-    """''', return_tensors="pt", return_attention_mask=False)
-
-    outputs = model.generate(**inputs, max_length=200)
-    text = tokenizer.batch_decode(outputs)[0]
-    print(text)
 
 
 def Mistral7x8BResponse(prompt):
@@ -105,25 +86,6 @@ def OpenAiAssistantResponse(prompt):
     return message_content.value
 
 
-def GeminiResponse(prompt):
-    import vertexai
-    from vertexai.preview.generative_models import GenerativeModel, Part
-
-    model = GenerativeModel("gemini-pro-vision")
-    responses = model.generate_content(
-        prompt,
-        generation_config={
-            "max_output_tokens": 2048,
-            "temperature": 0.4,
-            "top_p": 1,
-            "top_k": 32
-        },
-        stream=True,
-    )
-
-    for response in responses:
-        print(response.candidates[0].content.parts[0].text)
-
 
 if __name__ == "__main__":
-    GeminiResponse("Ping!")
+    print(Mistral7x8BResponse("Ping!"))
